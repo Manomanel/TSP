28ada867e4dca6c30f8d99da7d51ddbc1b7e7ac4
Manoel <manoel_am.neto@hotmail.com>
2023-08-28 13:55:47 -0300

    solucao

--- leitor-instancias/src/main.cpp
+++ leitor-instancias/src/main.cpp
@@ -1,5 +1,7 @@
-#include "Data.h"
 #include <iostream>
+
+#include "Data.h"
+#include "solucao.h"
 
 using namespace std;
 
@@ -7,21 +9,24 @@ int main(int argc, char** argv) {
 
     auto data = Data(argc, argv[1]);
     data.read();
-    size_t n = data.getDimension();
+    int dimensao = data.getDimension();
 
-    cout << "Dimension: " << n << endl;
+    cout << "Dimension: " << dimensao << endl;
     cout << "DistanceMatrix: " << endl;
     data.printMatrixDist();
 
+    //double matrizAdj[dimensao][dimensao];
+    //matrizAdj = data.getMatrixCost();
+    double **costMatrix = data.getMatrixCost();
 
     cout << "Exemplo de Solucao s = ";
     double cost = 0.0;
-    for (size_t i = 1; i < n; i++) {
+    for (int i = 1; i < dimensao; i++) {
         cout << i << " -> ";
         cost += data.getDistance(i, i+1);
     }
-    cost += data.getDistance(n, 1);
-    cout << n << " -> " << 1 << endl;
+    cost += data.getDistance(dimensao, 1);
+    cout << dimensao << " -> " << 1 << endl;
     cout << "Custo de S: " << cost << endl;
 
     return 0;
